c28bdbb376cc5d28560faf0bc8e8724b4647e944
Coded <coded@hrznstudio.com>
2019-09-26 17:29:03 +0100

    :bug: Fix compile errors

--- src/main/java/org/sandboxpowered/sandbox/api/block/Plant.java
+++ src/main/java/org/sandboxpowered/sandbox/api/block/Plant.java
@@ -1,14 +1,13 @@
 package org.sandboxpowered.sandbox.api.block;
 
-import com.hrznstudio.sandbox.api.entity.player.Player;
-import com.hrznstudio.sandbox.api.item.IItem;
-import com.hrznstudio.sandbox.api.item.ItemStack;
-import com.hrznstudio.sandbox.api.state.BlockState;
-import com.hrznstudio.sandbox.api.util.Mono;
-import com.hrznstudio.sandbox.api.world.World;
-import jdk.nashorn.internal.ir.Block;
+import org.sandboxpowered.sandbox.api.entity.player.PlayerEntity;
+import org.sandboxpowered.sandbox.api.item.Item;
+import org.sandboxpowered.sandbox.api.item.ItemStack;
 import org.sandboxpowered.sandbox.api.item.PlantSeed;
+import org.sandboxpowered.sandbox.api.state.BlockState;
+import org.sandboxpowered.sandbox.api.util.Mono;
 import org.sandboxpowered.sandbox.api.util.math.Position;
+import org.sandboxpowered.sandbox.api.world.World;
 import org.sandboxpowered.sandbox.api.world.WorldReader;
 
 import javax.annotation.Nullable;
@@ -23,13 +22,13 @@ public interface Plant extends Block {
     /**
      * @return Empty mono if can be planted, otherwise mono with the correct failure
      */
-    Mono<PlantableFailure> canBePlanted(WorldReader world, Position position);
+    Mono<PlantingFailure> canBePlanted(WorldReader world, Position position);
 
-    Mono<PlantableFailure> placePlant(WorldReader world, Position position, ItemStack seeds, Mono<Player> player);
+    Mono<PlantingFailure> placePlant(WorldReader world, Position position, ItemStack seeds, Mono<PlayerEntity> player);
 
-    boolean canHarvest(WorldReader world, Position position, BlockState state, Mono<Player> player);
+    boolean canHarvest(WorldReader world, Position position, BlockState state, Mono<PlayerEntity> player);
 
-    Set<ItemStack> harvest(World world, Position position, BlockState state, Mono<Player> player);
+    Set<ItemStack> harvest(World world, Position position, BlockState state, Mono<PlayerEntity> player);
 
     boolean fertilize(World world, Position position, BlockState state, ItemStack fertilizer);
 
@@ -37,11 +36,11 @@ public interface Plant extends Block {
 
     @Nullable
     @Override
-    default IItem asItem() {
-        return asSeed();
+    default Mono<Item> asItem() {
+        return Mono.of(asSeed());
     }
 
-    enum PlantableFailure {
+    enum PlantingFailure {
         OTHER,
         LIGHT,
         SPACE,
